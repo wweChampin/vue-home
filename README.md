11194b0e7cd91d83e53eb7ce8487de25ab379f97
champin1 <937524664@qq.com>
2018-07-18 13:48:49 +0800

    add file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vue-home
 王伟的音乐播放器
+
+webpack  base  可以配置别名
+比如 'common' : resolve('src/common')
+
+安装 stylus  和 stylus-loader （css预编译）ddd
+安装 fastclick  解决移动端点击事件的三秒延迟
+安装 babel-polyfill  给es6 的api  做个转义（提高兼容度）
+npm install babel-polyfill
+
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import FastClick from 'fastclick'
+import './common/stylus/index.styl'
 Vue.config.productionTip = false
+FastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
