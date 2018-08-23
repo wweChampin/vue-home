b1a35534a27b4d8ac7b916c5ce39bdffc85fa89a
unknown <bennettyw@bestseller.com.cn>
2018-08-23 16:45:28 +0800

    播放器vuex

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,5 +1,6 @@
+//播放模式的配置
 export const playMode = {
-  sequence: 0,
-  loop: 1,
-  random: 2
+  sequence: 0,  //顺序播放
+  loop: 1,   //循环播放
+  random: 2  // 随机播放
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,8 +1,15 @@
 /**
  * Created by bennettyw on 2018/8/10.
  */
+import {playMode} from  'common/js/config'
 const state = {
-    singer: {}
+    singer: {},
+    playing: false,
+    fullScreen: false,  //全屏
+    playlist: [],
+    sequenceList: [], //播放顺序列表
+    mode: playMode.sequence,
+    currentIndex: -1   //前进后退播放
 }
 
 export default state
